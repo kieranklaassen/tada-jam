c701e484493046f4c26fc8fe5b1a75cba926b41a
Kieran Klaassen <kieranklaassen@gmail.com>
2026-09-23 16:12:08 -0700

    Moon Phases declares ages 6-10; README lists both new games' bands (Bad Neighbours 4-8)

--- games/moon-phases/manifest.ts
+++ games/moon-phases/manifest.ts
@@ -3,7 +3,7 @@ import type { CartridgeManifest } from '../types'
 export const moonPhasesManifest = {
   key: 'moon-phases',
   name: 'Moon Phases',
-  ageBand: [5, 10],
+  ageBand: [6, 10],
   permissions: ['storage'],
   iconIdentity: { family: 'learn', contrast: 'paper' },
   windowShape: 'full',
